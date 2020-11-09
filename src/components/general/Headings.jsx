2e8a2bfe638adc8c578ee67c9ea2e865ad0325d8
Michael Mykhaylov <mikemikhailov2003@gmail.com>
2020-11-09 22:55:08 +0100

    Fixed UI bugs

--- src/components/general/Headings.jsx
+++ src/components/general/Headings.jsx
@@ -4,6 +4,7 @@ import { darkGrayColor } from '../../constants/websiteColors';
 export const Heading1 = styled.h1`
   margin: 0;
   font-family: 'Raleway', sans-serif;
+  font-weight: 900;
   font-style: normal;
   font-variation-settings: 'wght' 900;
   font-size: 1.75rem;
@@ -14,6 +15,7 @@ export const Heading1 = styled.h1`
 export const Heading2 = styled.h2`
   margin: 0;
   font-family: 'Raleway', sans-serif;
+  font-weight: 800;
   font-style: normal;
   font-variation-settings: 'wght' 800;
   font-size: 2.5rem;
@@ -23,6 +25,7 @@ export const Heading2 = styled.h2`
 export const Heading3 = styled.h3`
   margin: 0;
   font-family: 'Raleway', sans-serif;
+  font-weight: 700;
   font-style: normal;
   font-variation-settings: 'wght' 700;
   font-size: 2rem;
@@ -32,6 +35,7 @@ export const Heading3 = styled.h3`
 export const Heading4 = styled.h4`
   margin: 0;
   font-family: 'Raleway', sans-serif;
+  font-weight: 600;
   font-style: normal;
   font-variation-settings: 'wght' 600;
   font-size: 1.5rem;
@@ -41,6 +45,7 @@ export const Heading4 = styled.h4`
 export const Heading5 = styled.h5`
   margin: 0;
   font-family: 'Raleway', sans-serif;
+  font-weight: 500;
   font-style: normal;
   font-variation-settings: 'wght' 500;
   font-size: 1rem;
@@ -50,7 +55,8 @@ export const Heading5 = styled.h5`
 
 export const Text = styled.p`
   margin: 0;
-  font-family: 'Heebo';
+  font-family: 'Heebo', sans-serif;
+  font-weight: 400;
   font-style: normal;
   font-variation-settings: 'wght' 400;
   font-size: 1rem;
